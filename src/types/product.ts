47c548a8cad44fd0ea172e3da983e38722bc3afa
Jose Miguel <jose.miguelq95@gmail.com>
2025-07-13 20:56:23 -0300

    types

---
+++ src/types/product.ts
@@ -0,0 +1,17 @@
+export interface Variation {
+  price: number | "";
+  description: string;
+  images: string[];
+  showUrlInput?: boolean;
+  tempImageUrl?: string;
+}
+
+export interface ProductFormData {
+  name: string;
+  model: string;
+  type: string;
+  brand: string;
+  stock: string;
+  notes: string;
+  variations: Variation[];
+} 
